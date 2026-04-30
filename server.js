992fd4a9d851f291032ab5b918019a369fc3153d
jequinonesl-123 <62910288+jequinonesl-123@users.noreply.github.com>
2026-04-29 22:48:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.get("/test-db", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
